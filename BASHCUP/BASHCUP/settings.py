fbeb0b692d22cdb15db4821415a20d2f08657a1e
Sujan Pokharel <sujanpokharel04@example.com>
2024-12-12 20:18:28 +0545

    renderhostingtry

--- BASHCUP/BASHCUP/settings.py
+++ BASHCUP/BASHCUP/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8j6ubznxud&a=%$eos&qzp6l4!*zbx6!^yx%#vjriuym&!&bml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bashcup.onrender.com']
 
 
 # Application definition
